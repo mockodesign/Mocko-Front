997b87c862cd7ed137dd74f579e90ec53a4a3f1c
mockodesign <mockodesign@gmail.com>
2025-10-21 21:59:17 +0300

    Update design-list.js

--- src/components/home/design-list.js
+++ src/components/home/design-list.js
@@ -137,8 +137,14 @@ function DesignList({
                   }}
                   className="aspect-square w-full border-2 border-slate-200 rounded-2xl overflow-hidden cursor-pointer hover:border-blue-400 transition-all duration-300 group shadow-lg hover:shadow-2xl bg-white"
                 >
-                  {design?.canvasData ? (
-                    <DesignPreview key={design._id} design={design} />
+                  {/* PERFORMANCE FIX: Always use thumbnail for list view - much faster than rendering full canvasData */}
+                  {design?.thumbnail ? (
+                    <img
+                      src={design.thumbnail}
+                      alt={design.name}
+                      className="w-full h-full object-cover"
+                      loading="lazy"
+                    />
                   ) : (
                     <DesignThumbnail design={design} />
                   )}
